ef49e2dc7db9ccf524853c43c2ecf0b41b172768
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-01-11 15:51:34 -0700

    init

---
+++ app/week-4/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewItem from './new-item';
+
+
+const Page = () => {
+    return (
+      <main  >
+        <NewItem/>
+      </main>
+    );
+  };
+  
+  export default Page;
