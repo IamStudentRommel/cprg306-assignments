c365873ac02282e0f26fc06b1e5f531390ecdf24
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-01-11 16:04:27 -0700

    almost done week 4

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -24,62 +24,55 @@ const NewItem = () => {
   };
 
   return (
-    <main class="flex justify-center w-full">
-        <form onSubmit={handleSubmit} >
-        <div className="p-2 m-4 bg-slate-900 text-black max-w-sm w-full">
-            <div className='class="mb-2"' >
-
-                <label className='text-gray-600 font-bold   pb-2  '>
-                <input
-                    placeholder="Item name"
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                    className="w-full mt-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
-                />
-                </label>
-
-
-            <div className="flex justify-between">
-                <div className='w-1/2 '>
-                <label className='text-gray-600 font-bold   pb-2'>
+    <form onSubmit={handleSubmit} >
+        <main class="flex justify-center w-full">
+            <div className="p-2 m-4 bg-slate-900 text-black max-w-sm w-full">
+                <div className="mb-2" >
                     <input
-                    type="number"
-                    min="1"
-                    max="99"
-                    value={quantity}
-                    onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
-                    required
-                    className="w-20 ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
+                        placeholder="Item name"
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                        className="w-full mt-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
                     />
-                </label>
+                </div>
+                <div className="flex justify-between">
+                    <div className='w-1/2 '>
+                        <input
+                        type="number"
+                        min="1"
+                        max="99"
+                        value={quantity}
+                        onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
+                        required
+                        className="w-20 ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
+                        />
 
+                    </div>
+                    <div className='flex justify-between'>
+                        <select className="ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
+                        value={category} onChange={(e) => setCategory(e.target.value)}>
+                        <option value="produce">Produce</option>
+                        <option value="dairy">Dairy</option>
+                        <option value="Bakery">Bakery</option>
+                        <option value="Meat">Meat</option>
+                        <option value="Frozen Foods">Frozen Foods</option>
+                        <option value="Canned Goods">Canned Goods</option>
+                        <option value="Dry Goods">Dry Goods</option>
+                        <option value="Beverages">Beverages</option>
+                        <option value="Snacks">Snacks</option>
+                        <option value="Household">Household</option>
+                        <option value="Other">Other</option>
+                        </select>
+                    </div>
                 </div>
-                <div className='flex justify-between'>
-                    <select className="ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
-                    value={category} onChange={(e) => setCategory(e.target.value)}>
-                    <option value="produce">Produce</option>
-                    <option value="dairy">Dairy</option>
-                    <option value="Bakery">Bakery</option>
-                    <option value="Meat">Meat</option>
-                    <option value="Frozen Foods">Frozen Foods</option>
-                    <option value="Canned Goods">Canned Goods</option>
-                    <option value="Dry Goods">Dry Goods</option>
-                    <option value="Beverages">Beverages</option>
-                    <option value="Snacks">Snacks</option>
-                    <option value="Household">Household</option>
-                    <option value="Other">Other</option>
-                    </select>
-                </div>
+                <button type="submit" className="w-full mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                    +
+                </button>
             </div>
-            <button type="submit" className="w-full mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
-                +
-            </button>
-            </div>
-        </div>
-        </form>
-    </main>
+        </main>
+    </form>
   );
 };
 
