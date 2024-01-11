6eb995cfe99aecf5dbd97141709a55414c42db2a
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-01-11 11:07:26 -0700

    test

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import Link from 'next/link';
-// import StudentInfo from './student-info';
-=======
 import StudentInfo from './student-info';
->>>>>>> origin/main
 export default function Page() {
     return (
       <main>
