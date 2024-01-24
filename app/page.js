1c5480eb3b47752a1052ccb7ca376fc2928bfedd
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-01-23 20:14:43 -0700

    init

--- app/page.js
+++ app/page.js
@@ -7,8 +7,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
-      <h1 className="text-4xl font-bold mb-5">SyntaxMel</h1>
-        <h1 className="text-2xl font-bold mb-5">Web Development 2 Assignments</h1>
+        <h1 className="text-4xl font-bold mb-5">SyntaxMel</h1>
+        <h1 className="text-2xl font-bold mb-5">
+          Web Development 2 Assignments
+        </h1>
         <div className="text-lg">
           <ul>
             {weekDemos.map((week) => (
--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,14 +1,14 @@
-export default function Item({name, quantity, category}){
-    return(
-        <div>
-            <ul>
-                <li class="p-2 m-4 bg-slate-900 max-w-sm">
-                    <h2 class="text-xl font-bold">{name}</h2>
-                    <div class="text-sm">
-                        Buy {quantity} in {category}
-                    </div>
-                </li>
-                </ul>
-        </div>
-    );
+export default function Item({ name, quantity, category }) {
+  return (
+    <div>
+      <ul>
+        <li class="p-2 m-4 bg-slate-900 max-w-sm">
+          <h2 class="text-xl font-bold">{name}</h2>
+          <div class="text-sm">
+            Buy {quantity} in {category}
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
