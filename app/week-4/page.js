7b9c0c842ccfea738a0a04cb7316a4815b3a5a78
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-01-28 21:41:15 -0700

    working with challenge task

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import NewItem from './new-item';
-
+import React from "react";
+import NewItem from "./new-item";
 
 const Page = () => {
-    return (
-      <main  >
-        <NewItem/>
-      </main>
-    );
-  };
-  
-  export default Page;
+  return (
+    <main>
+      <NewItem />
+    </main>
+  );
+};
+
+export default Page;
