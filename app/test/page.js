2f76e37413878622403a2021bfe2963d33abcf3e
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-01-30 13:34:25 -0700

    blah

---
+++ app/test/page.js
@@ -0,0 +1,8 @@
+export default function Test123() {
+  return (
+    <main>
+      <h1>asdasd</h1>
+      <h1>123123123</h1>
+    </main>
+  );
+}
--- app/test/tester.js
+++
@@ -1,8 +0,0 @@
-export default function Test123(){
-    return(
-        <main>
-            <h1>asdasd</h1>
-            <h1>123123123</h1>
-        </main>
-    )
-}
