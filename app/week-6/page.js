b2c213754637b12b36bdb111af5d6f61b9c854ba
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-02-16 14:35:29 -0700

    update bug

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -14,7 +14,7 @@ const Page = () => {
   return (
     <main class="bg-slate-950 p-2 m-2">
       <div className=" max-w-sm w-full">
-        <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
+        <h1 className="text-3xl font-bold mb-4">Shopping Listasd</h1>
         <h3 class="text-xl font-bold">Add New Item</h3>
         <NewItem onAddItem={handleAddItem} />
         <ItemList items={items} />
