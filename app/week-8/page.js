93d3d5e5b4f5a64aee9d8545e73a70805149ffe1
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-03-04 18:35:47 -0700

    s

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -13,6 +13,7 @@ export default function Page() {
     await firebaseSignOut();
   };
   // console.log(user);
+
   return (
     <main>
       <h1 class="text-4xl font-bold mb-5">Shopping List App</h1>
