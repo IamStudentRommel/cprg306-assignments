f3c37cf42b4a7dc1931d08c8c89751ff05d58ec1
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-03-04 18:14:03 -0700

    vercel conn

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -12,7 +12,7 @@ export default function Page() {
   const handleLogout = async () => {
     await firebaseSignOut();
   };
-
+  console.log(user);
   return (
     <main>
       <h1 class="text-4xl font-bold mb-5">Shopping List App</h1>
